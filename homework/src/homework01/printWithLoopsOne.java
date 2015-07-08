c00aab9e1df26ba6ede7cae760241745e8451e84
elacang <elacang@gmail.com>
2015-07-08 09:49:50 +0800

    homework1 done

---
+++ homework/src/homework01/printWithLoopsOne.java
@@ -0,0 +1,28 @@
+package homework01;
+
+public class printWithLoopsOne {
+	
+	public static void main(String[] args) {
+    
+	for(int i=1; i<=5 ;i++){
+	           
+            for(int j=0; j < i; j++){
+                    System.out.print(j+1);
+            }
+           
+            System.out.println("");
+    }
+		
+		
+	for(int i=4; i>0 ;i--){
+           
+            for(int j=0; j < i; j++){
+                    System.out.print(j+1);
+            }
+           
+            System.out.println("");
+    }
+
+}
+
+}
